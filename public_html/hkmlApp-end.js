a59acc980c9e045e3899d92a8e10b7d1ef624468
BROTHERSEVEN <brotherseven@gmail.com>
2017-10-31 01:10:21 +0800

    - add end of file js

---
+++ public_html/hkmlApp-end.js
@@ -0,0 +1,13 @@
+var allLinks = document.getElementsByTagName('a'); 
+if (allLinks) {
+    var i;
+    for (i=0; i<allLinks.length; i++) {
+        var link = allLinks[i];
+        var target = link.getAttribute('target'); 
+        if (target && target == '_blank') {
+            link.setAttribute('target','_self');
+        }
+    }
+}
+
+showPopupText = function(){}
