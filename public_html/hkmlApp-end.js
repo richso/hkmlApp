33100d63a141e24863868956ded7ee4ad3dbfb46
BROTHERSEVEN <brotherseven@gmail.com>
2017-11-10 12:38:16 +0800

    - change link color

--- public_html/hkmlApp-end.js
+++ public_html/hkmlApp-end.js
@@ -1,5 +1,5 @@
 
-$j('a[href][target]').attr('target', '_self').css('color', 'blue')
+$j('.t_row > tbody > tr > td:nth-child(2) a[href][target]').attr('target', '_self').css('color', 'blue')
 
 //setTimeout(function(){
     showPopupText = function(){}
