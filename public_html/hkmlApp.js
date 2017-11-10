ff017f1d20f953f13e4d48350f798ae2bc54474b
BROTHERSEVEN <brotherseven@gmail.com>
2017-11-10 22:43:16 +0800

    - test cater the post form

--- public_html/hkmlApp.js
+++ public_html/hkmlApp.js
@@ -88,7 +88,10 @@ $j(document).ready(function() {
             $('a[href^="misc.php"]').parent('div').removeClass('right').appendTo(p[0]);
             var tube = $('a[href^="https://www.youtube.com/"]').attr('href'); 
             tube = tube.substring(32).split('&')[0]; 
-            $('a[href^="https://www.youtube.com/"]').replaceWith('<iframe width="560" height="315" src="https://www.youtube.com/embed/'+tube+'" frameborder="0" allowfullscreen></iframe>')
+            var w = $(window).width();
+            var vw = w * 0.8;
+            var vh = vw * 315 / 560;
+            $('a[href^="https://www.youtube.com/"]').replaceWith('<iframe width="'+vw+'" height="'+vh+'" src="https://www.youtube.com/embed/'+tube+'" frameborder="0" allowfullscreen></iframe>')
         }    
         
         if (/^my\.php$/.test(lastLocSeg)) { 
