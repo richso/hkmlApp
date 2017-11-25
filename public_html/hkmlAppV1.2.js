0f8b43d406de62c79cb8ddc67a23d1e04de10295
BROTHERSEVEN <brotherseven@gmail.com>
2017-11-26 01:43:51 +0800

    - adding v1.2 (incomplete) for testing

--- public_html/hkmlAppV1.2.js
+++ public_html/hkmlAppV1.2.js
@@ -22,7 +22,7 @@ $j(document).ready(function() {
                 (hashSeg ? hashSeg.length+1 : 0) - 
                 (lastLocParams ? lastLocParams.length+1 :0)
         );
-        alert(location.href + ": " + location.href.length + ',' + hashSeg.length + "," + lastLocSeg.length + "," + lastLocParams.length + ":" + locBase);
+        alert(location.href + ": " + location.href.length + ',' + hashSeg.length + "," + lastLocSeg + "," + lastLocParams.length + ":" + locBase);
         
         var mainTable_q = $('body > center > .maintable');
     
