4ee7de12c7b0210e5d31f810d0e10af9d6ff0874
BROTHERSEVEN <brotherseven@gmail.com>
2018-01-07 23:11:48 +0800

    - experimental: send product thumbnails to App for lightbox on click

--- public_html/hkmlApp.js
+++ public_html/hkmlApp.js
@@ -356,9 +356,16 @@ $j(document).ready(function() {
             });
             
             try {
-                var thumbnails = $('.t_row > tbody > tr > td:nth-child(2) img:not([smilieid]):not([src^="images/d-xite"]):not([src^="images/common"]):not([src^="http://www.hkml.net/Discuz/images/common"]):not([src^="http://hkml.net/Discuz/images/common"]):not([src^="images/attachicons"]):not([src^="http://wpa.qq.com/pa?p="]):not([src^="http://web.icq.com/whitepages/online?icq="]):not([src^="http://edit.yahoo.com/config/send_webmesg?.target="]):not([src^="http://blog.roodo.com/onion_club/"]):not([src^="http://amos1.taobao.com/"])');
                 if (window.webkit && window.webkit.messageHandlers && typeof window.webkit.messageHandlers.hkmlAppThnumbnail != "undefined") {
-                    window.webkit.messageHandlers.hkmlAppThnumbnail.postMessage(thumbnails);
+                    var thumbnails = $('.t_row > tbody > tr > td:nth-child(2) img:not([smilieid]):not([src^="images/d-xite"]):not([src^="images/common"]):not([src^="http://www.hkml.net/Discuz/images/common"]):not([src^="http://hkml.net/Discuz/images/common"]):not([src^="images/attachicons"]):not([src^="http://wpa.qq.com/pa?p="]):not([src^="http://web.icq.com/whitepages/online?icq="]):not([src^="http://edit.yahoo.com/config/send_webmesg?.target="]):not([src^="http://blog.roodo.com/onion_club/"]):not([src^="http://amos1.taobao.com/"])');
+                    thumbnails.each(function(idx, n){
+                        $(n)[0].onclick = function(){
+                            window.webkit.messageHandlers.hkmlAppThnumbnail.postMessage({
+                                idx: idx,
+                                images: thumbnails
+                            });
+                        }
+                    });
                 }
                 
             } catch (e) {
