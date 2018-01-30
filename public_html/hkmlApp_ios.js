64b99842263ffc112cf95026064f7f02a227b4be
BROTHERSEVEN <brotherseven@gmail.com>
2018-01-30 22:03:26 +0800

    - sync all versions

--- public_html/hkmlApp_ios.js
+++ public_html/hkmlApp_ios.js
@@ -514,16 +514,25 @@ $j(document).ready(function() {
         }
         
         try {
-            if (!appvl_flag && userAgent.match(/iPhone/i)) {
+            if (userAgent.match(/iPhone/i)) {
                 $('<div style="height: 60px;"></div>').appendTo('body');
-                var d = $('<div style="position: fixed; bottom:0; width: calc(100% - 40px); height: 60px; background-color: #dddddd; padding: 0 20px;"></div>')
-                        .append('<a href="javascript:void(0);" onclick="window.history.back()" style="float: left; padding: 5px; font-size: 24px;">&#8617;</a>')
-                        .append('<a href="javascript:void(0);" onclick="window.history.forward()" style="float: left; padding: 5px; font-size: 24px;">&#8618;</a>')
-                        .append('<a href="javascript:void(0);" onclick="location=\'./index.php\';" style="float: right; padding: 5px; font-size: 24px;">&#127968;</a>')
-                        .append('<a href="facebookshare:'+location.href+'" style="float: right; padding: 5px; font-size: 24px;">&#9734;</a>')
-                        .append('<div style="clear: both;"></div>');
+                var d;
+                if (!appvl_flag) {
+                    d = $('<div style="position: fixed; bottom:0; width: calc(100% - 40px); height: 60px; background-color: #dddddd; padding: 0 20px;"></div>')
+                            .append('<a href="javascript:void(0);" onclick="window.history.back()" style="float: left; padding: 5px; font-size: 24px;">&#8617;</a>')
+                            .append('<a href="javascript:void(0);" onclick="window.history.forward()" style="float: left; padding: 5px; font-size: 24px;">&#8618;</a>')
+                            .append('<a href="javascript:void(0);" onclick="location=\'./index.php\';" style="float: right; padding: 5px; font-size: 24px;">&#127968;</a>')
+                            .append('<a href="facebookshare:'+location.href+'" style="float: right; padding: 5px; font-size: 24px;">&#9734;</a>')
+                            .append('<div style="clear: both;"></div>');
+                }
+                if (!usrname) {
+                    d = $('<div style="position: fixed; bottom:0; width: calc(100% - 40px); height: 60px; background-color: #dddddd; padding: 0 20px;"></div>')
+                            .append('<a href="logging.php?action=login" style="padding-top: 5px;">µn¤J</a>')
+                            .append('<div style="clear: both;"></div>');
+                }
                 d.appendTo('body');
             }
+            
         } catch (e) {}
 
         function alterReplyBox() {
