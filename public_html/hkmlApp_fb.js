5178247698ad145a300b321b2394174bd8b4094c
BROTHERSEVEN <brotherseven@gmail.com>
2018-02-08 19:00:52 +0800

    - added hkmlApp_fb.js for adding a nav panel in facebook page

---
+++ public_html/hkmlApp_fb.js
@@ -0,0 +1,25 @@
+$j(document).ready(function() {
+    var $ = $j;
+    
+    var userAgent = window.navigator.userAgent;
+    
+    var appHead = $('html').attr('hkmlApp_head');
+    
+    if (! appHead) {
+        $('html').attr('hkmlApp_head', 1);
+        
+        try {
+            var d = $('<div style="position: fixed; bottom:0; width: calc(100% - 40px); height: 60px; background-color: #eeeeee; padding: 0 20px;"></div>')
+                .append('<a href="javascript:void(0);" onclick="window.history.back()" style="float: left; padding: 5px; font-size: 24px;">&#9664;</a>')
+                .append('<a href="javascript:void(0);" onclick="window.history.forward()" style="float: left; padding: 5px; font-size: 24px;">&#9654;</a>')
+                .append('<a href="javascript:void(0);" onclick="location=\'./index.php\';" style="float: right; padding: 5px; font-size: 24px;">&#127968;</a>')
+                .append('<a href="facebookshare:'+location.href+'" style="float: right; padding: 5px; font-size: 24px;">&#9734;</a>');
+            
+            d.append('<div style="clear: both;"></div>');
+
+            $('<div style="height: 60px;"></div>').appendTo('body');
+            d.appendTo('body');
+        } catch (e) {}
+    }
+})
+
