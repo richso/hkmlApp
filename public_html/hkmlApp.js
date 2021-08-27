bb0f85ac477dc504bccc768c20c6da143ebc7218
BROTHERSEVEN <brotherseven@gmail.com>
2021-08-27 16:55:52 +0800

    - add new links 精 and 新 - minor layout adjusting

--- public_html/hkmlApp.js
+++ public_html/hkmlApp.js
@@ -162,7 +162,7 @@ $j(document).ready(function() {
         if (/^digest\.php$/.test(lastLocSeg)) {
             $('td.subject a[target="_blank"]').attr('target', '_self');
             $('td a[href^="forumdisplay"][target="_blank"]').attr('target', '_self');
-            $('> td:nth-child(1)', $('tr.header')).attr('width', '41%');
+            $('> td:nth-child(1)', $('tr.header')).attr('width', '65%');
             $('> td:nth-child(3)', $('tr.header')).attr('width', '19%');
             $('> td:nth-child(4)', $('tr.header')).css('display', 'none');
             $('> td:nth-child(5)', $('tr.header')).css('display', 'none');
