2beb325d01bef4c38b41e71faa47e9fc6035a3e7
BROTHERSEVEN <brotherseven@gmail.com>
2026-09-25 13:35:39 +0800

    Added support for on the fly resize / rotate / convert to jpg / rename to .jpg on submit - change some common Chinese to entity coding (&x9999;) - change message multibyte words to entity coding - make exception for the form of short msg which have no image submission

--- public_html/hkmlApp.js
+++ public_html/hkmlApp.js
@@ -315,7 +315,7 @@ $j(document).ready(function() {
             });
             
             try {
-                var thumbnails = $('.t_row > tbody > tr > td:nth-child(2) img:not([smilieid]):not([src^="images/d-xite"]):not([src^="images/common"]):not([src^="http://www.hkml.net/Discuz/images/common"]):not([src^="http://hkml.net/Discuz/images/common"]):not([src^="images/attachicons"]):not([src^="http://wpa.qq.com/pa?p="]):not([src^="http://web.icq.com/whitepages/online?icq="]):not([src^="http://edit.yahoo.com/config/send_webmesg?.target="]):not([src^="http://blog.roodo.com/onion_club/"]):not([src^="http://amos1.taobao.com/"])');
+                var thumbnails = $('.t_row > tbody > tr > td:nth-child(2) img:not([smilieid]):not([src^="images/d-xite"]):not([src^="images/common"]):not([src^="https://www.hkml.net/Discuz/images/common"]):not([src^="https://hkml.net/Discuz/images/common"]):not([src^="images/attachicons"]):not([src^="http://wpa.qq.com/pa?p="]):not([src^="http://web.icq.com/whitepages/online?icq="]):not([src^="http://edit.yahoo.com/config/send_webmesg?.target="]):not([src^="http://blog.roodo.com/onion_club/"]):not([src^="http://amos1.taobao.com/"])');
                 var urls = [];
                 thumbnails.each(function(idx, n){
                     urls.push($(n).attr('src'));
